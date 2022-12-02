1e3c33fa5b4d194be16cfb39f4d0f6c17b83828b
eric <ericarc99@gmail.com>
2022-12-02 17:14:49 +0800

    初始化樣式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Weibo App') - Weibo 9</title>
+    <title>@yield('title', 'Weibo 9 App') - Weibo 9</title>
+    <link rel="stylesheet" href="css/app.css">
+    <!-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> -->
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo 9 App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">幫助</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">登入</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主頁</h1>
+    <div class="bg-light p-3 p-sm-5 rounded">
+        <h1>Hello everyone !</h1>
+        <p class="lead">
+            歡迎光臨 Weibo 9 網站
+        </p>
+        <p>
+            讓我們一起從這裡出發吧 ! GO !
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">歡迎註冊</a>
+        </p>
+    </div>
 @stop
