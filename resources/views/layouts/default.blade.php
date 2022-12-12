bae259bc37c3007023a05f3d9eac126f9760971e
eric <ericarc99@gmail.com>
2022-12-12 17:31:46 +0800

    命名路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Weibo 9 App') - Weibo 9</title>
-    <link rel="stylesheet" href="css/app.css">
+    <!-- <link rel="stylesheet" href="css/app.css"> -->
     <!-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> -->
-    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('layouts._header')
