b4df673385f9c8e3c3b3470c8e3ac7efe421b675
eric <ericarc99@gmail.com>
2023-01-06 16:34:26 +0800

    登錄時記住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             //登入成功
             session()->flash('success', '歡迎回來 !');
             return redirect()->route('users.show', [Auth::user()]);
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,7 +16,7 @@
               <form action="{{ route('logout') }}" method="POST">
                 {{ csrf_field() }}
                 {{ method_field('DELETE') }}
-                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                <button class="btn btn-block btn-danger" type="submit" name="button">登出</button>
               </form>
             </a>
           </div>
