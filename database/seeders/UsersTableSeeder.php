173088efa57bbb33202f0de560a09113b9c8e508
eric <ericarc99@gmail.com>
2023-01-18 17:26:54 +0800

    管理員可刪除用戶

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Eric';
         $user->email = 'ericarc99@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
